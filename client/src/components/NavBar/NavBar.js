85f1af0afffb38792a1cb8dcee08d8332259733a
Austin Koeppel <Austin.Koeppel@gmail.com>
2018-08-24 21:49:22 -0400

    [Austin Koeppel][Pushing up new styling for login and landing]

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const NavBar = () => (
+  <header>
+    <nav>
+      <div class="nav-wrapper">
+        <ul className="right hide-on-small-and-down" id="nav-mobile">
+          <li className="hide-on-small-only">
+            <a href='/register'>  Register</a>
+          </li>
+          <li className="hide-on-small-only">
+            <a href='/Login'>  Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+  
+export default NavBar;
